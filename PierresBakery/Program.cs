161a8526378ede2ab7dcd6d9cf731fb274bfa868
Liam Eller <liamthelastson@gmail.com>
2022-02-25 16:29:23 -0800

    Refactor to add running price

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -6,26 +6,28 @@ namespace PierresBakery
   public class Bakery
   {
     public static void Main()
-    {
+    { 
       Bread bread = new Bread();
       Pastry pastry = new Pastry();
       bool boolean = true;
+      int breadCost = 0;
+      int pastryCost = 0;
 
       Console.WriteLine("Welcome to Pierre's Bakery");
       Console.WriteLine("Would you like to order some of my tasty pastries or breads?");
       string answer = Console.ReadLine();
       if (answer.Contains("yes"))
       {
+        Console.WriteLine("Excellent!");
+        Console.WriteLine("How many loafs of bread would you like?");
+        int loafs = int.Parse(Console.ReadLine());
+        Console.WriteLine("and how many pastries would you like?");
+        int pastries = int.Parse(Console.ReadLine());
+        breadCost += bread.CostOfBread(loafs);
+        pastryCost += pastry.CostOfPastry(pastries);
+        Console.WriteLine("Fantastic. That'll be $"+breadCost+" for the bread, and $"+pastryCost+" for the pastries");
         while (boolean == true)
         {
-          Console.WriteLine("Excellent!");
-          Console.WriteLine("How many loafs of bread would you like?");
-          int loafs = int.Parse(Console.ReadLine());
-          Console.WriteLine("and how many pastries would you like?");
-          int pastries = int.Parse(Console.ReadLine());
-          int breadCost = bread.CostOfBread(loafs);
-          int pastryCost = pastry.CostOfPastry(pastries);
-          Console.WriteLine("Fantastic. That'll be $"+breadCost+" for the bread, and $"+pastryCost+" for the pastries");
           Console.WriteLine("");
           Console.WriteLine("Do you want to order more?");
           string orderAgain = Console.ReadLine();
@@ -34,6 +36,18 @@ namespace PierresBakery
             Console.WriteLine("Ok see you next time");
             Console.WriteLine("Bye now!");
             boolean = false;
+          }
+          else
+          {
+            Console.WriteLine("Excellent!");
+            Console.WriteLine("How many loafs of bread would you like?");
+            loafs = int.Parse(Console.ReadLine());
+            Console.WriteLine("and how many pastries would you like?");
+            pastries = int.Parse(Console.ReadLine());
+            breadCost += bread.CostOfBread(loafs);
+            pastryCost += pastry.CostOfPastry(pastries);
+            Console.WriteLine("Fantastic. Your new price is $"+breadCost+" for the bread, and $"+pastryCost+" for the pastries");
+            Console.WriteLine("");
           }
         }
       }
@@ -45,7 +59,7 @@ namespace PierresBakery
       }
       else
       {
-        Console.WriteLine("Don't be difficult. It's a yes or no answer");
+        Console.WriteLine("Don't be difficult. It's a simple yes or no answer");
       }
     }
   }
