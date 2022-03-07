64fbcbf752386580edc4083290be3d7c2430783f
Liam Eller <liamthelastson@gmail.com>
2022-03-06 23:06:03 -0800

    Refactor pragram.cs

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,60 +7,67 @@ namespace PierresBakery
   {
     public static void Main()
     { 
-      Bread bread = new Bread();
-      Pastry pastry = new Pastry();
-      bool boolean = true;
-      int breadCost = 0;
-      int pastryCost = 0;
+      bool orderAgain = true;
 
       Console.WriteLine("Welcome to Pierre's Bakery");
-      Console.WriteLine("Would you like to order some of my tasty pastries or breads?");
+      Console.WriteLine("I'm Pierre");
+      Console.WriteLine("Whats your name?");
+      string name = Console.ReadLine();
+      Bread bread = new Bread(name);
+      Pastry pastry = new Pastry(name);
+      Console.WriteLine("Nice to meet you "+bread.Customer+".");
+      Console.WriteLine("would you like to order some of my tasty pastries or breads?");
       string answer = Console.ReadLine();
       if (answer.Contains("yes"))
       {
-        Console.WriteLine("Excellent!");
-        Console.WriteLine("How many loafs of bread would you like?");
-        int loafs = int.Parse(Console.ReadLine());
-        Console.WriteLine("and how many pastries would you like?");
-        int pastries = int.Parse(Console.ReadLine());
-        breadCost += bread.CostOfBread(loafs);
-        pastryCost += pastry.CostOfPastry(pastries);
-        Console.WriteLine("Fantastic. That'll be $"+breadCost+" for the bread, and $"+pastryCost+" for the pastries");
-        while (boolean == true)
+
+        Bakery.Order(bread, pastry);
+        Console.WriteLine("Okay, that'll be $"+bread.Cost+" for the bread, and $"+pastry.Cost+" for the pastries.");
+        Console.WriteLine("You got "+bread.Free+" loafs of bread & "+pastry.Free+" pastries for free!");
+        while (orderAgain == true)
         {
           Console.WriteLine("");
           Console.WriteLine("Do you want to order more?");
-          string orderAgain = Console.ReadLine();
-          if (orderAgain.Contains("no"))
+          string orderMore = Console.ReadLine();
+          if (orderMore.Contains("no"))
           {
-            Console.WriteLine("Ok see you next time");
-            Console.WriteLine("Bye now!");
-            boolean = false;
+            orderAgain = Goodbye(bread);
           }
           else
           {
-            Console.WriteLine("Excellent!");
-            Console.WriteLine("How many loafs of bread would you like?");
-            loafs = int.Parse(Console.ReadLine());
-            Console.WriteLine("and how many pastries would you like?");
-            pastries = int.Parse(Console.ReadLine());
-            breadCost += bread.CostOfBread(loafs);
-            pastryCost += pastry.CostOfPastry(pastries);
-            Console.WriteLine("Fantastic. Your new price is $"+breadCost+" for the bread, and $"+pastryCost+" for the pastries");
-            Console.WriteLine("");
+            Bakery.Order(bread, pastry);
+            Console.WriteLine("Fantastic! Your new price is $"+bread.Cost+" for the bread, and $"+pastry.Cost+" for the pastries.");
+            Console.WriteLine("You got "+bread.Free+" loafs of bread & "+pastry.Free+" pastries for free!");
           }
         }
       }
       else if (answer.Contains("no"))
       {
-        Console.WriteLine("Ok see you next time!");
-        Console.WriteLine("Bye now");
-        boolean = false;
+        orderAgain = Goodbye(bread);
       }
       else
       {
         Console.WriteLine("Don't be difficult. It's a simple yes or no answer");
       }
+    }
+    public static void Order(Bread bread, Pastry pastry)
+    {
+      Console.WriteLine("Excellent!");
+      Console.WriteLine("How many loafs of bread would you like?");
+      int loafs = int.Parse(Console.ReadLine());
+      Console.WriteLine("and how many pastries would you like?");
+      int pastries = int.Parse(Console.ReadLine());
+
+      bread.CostOfBread(loafs);
+      bread.FreeBread(loafs);
+      pastry.CostOfPastry(pastries);
+      pastry.FreePastry(pastries);
+    }
+    public static bool Goodbye(Bread bread)
+    {
+      Console.WriteLine("See you next time "+bread.Customer+"!");
+      Console.WriteLine("Bye now");
+      return false;
     }
   }
 }
